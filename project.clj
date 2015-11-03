5e7a1de80285812c90613bb565bdee19d423a453
Matt Krukowski <matthew@chartbeat.com>
2015-11-03 11:38:50 -0500

    release 0.1.2-CB

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.chartbeat.cljbeat/lein-assemble "0.1.2"
+(defproject com.chartbeat.cljbeat/lein-assemble "0.1.2-CB"
   :description "A leiningen plugin to do the job of maven assembly"
   :url "https://github.com/chartbeat-labs/lein-assembly"
   :license {:name "BSD 3 Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[me.raynes/fs "1.4.6"]
+  :dependencies [[me.raynes/fs "1.4.6-CB"]
                  [stencil "0.5.0" :exclusions [org.clojure/core.cache]]
                  [org.clojure/clojure "1.6.0"]
                  [org.apache.ant/ant "1.9.4"]]
