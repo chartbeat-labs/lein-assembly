0abb3452714a3c39550bcebb3ebf212120bfcaf8
Thorben Stangenberg <thorben@stangenberg.ch>
2017-11-14 22:27:51 +0100

    version 0.1.5 update to clojure 1.8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.chartbeat.cljbeat/lein-assemble "0.1.4"
+(defproject com.chartbeat.cljbeat/lein-assemble "0.1.5"
   :description "A leiningen plugin to do the job of maven assembly"
   :url "https://github.com/chartbeat-labs/lein-assembly"
   :license {:name "BSD 3 Clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[me.raynes/fs "1.4.6"]
                  [stencil "0.5.0" :exclusions [org.clojure/core.cache]]
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.apache.ant/ant "1.9.4"]]
   :deploy-repositories [["releases" :clojars]]
   :signing {:gpg-key "F0903068"}
